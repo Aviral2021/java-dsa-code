982be8a5e8467c63acbae3b835eb9c3bd9ad3eb6
aviral2021 <aviral.ece.ipu@gmail.com>
2025-09-24 23:05:16 +0530

    Initial commit

---
+++ src/javaconcepts/collections/ArrayListTest.java
@@ -0,0 +1,30 @@
+package javaconcepts.collections;
+import java.util.ArrayList;
+
+public class ArrayListTest {
+    public static void main(String[] args){
+
+        ArrayList<Integer> list = new ArrayList<>();
+        list.add(10);
+        list.add(20);
+
+        ArrayList<String> list1 = new ArrayList<>();
+
+        list1.add("Hello");
+        list1.add("Java");
+
+        System.out.println(list);
+        System.out.println(list1);
+
+        //get elements
+
+        int element = list.get(1);
+        System.out.println(element);
+
+        //add inbetween in list
+        list.add(1, 78);
+        System.out.println(list);
+
+    }
+
+}
